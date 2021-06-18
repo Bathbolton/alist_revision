56a330453f0604d50e649fc4885070ad3712f5d3
bathbolton <bathbolton@yahoo.com>
2021-06-18 10:55:06 +0100

    first commit

---
+++ home/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+# Create your views here.
+
+def index(request):
+    type = Type.objects.all()
+
+    context = {
+        'jibola':type
+    }
+    return render(request, 'index.html',context)
+    # return HttpResponse('App installed')
+
+def food(request):
+    prep = Preparation.objects.filter(type_id=id)   
+
+    context = {
+        'barth':prep
+    }
+
+    return render(request, 'food.html', context)
